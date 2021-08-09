9cbb2dea79a7af77703abf40a975e9b0dbaa7f3b
Kenny Miesner <84544990+kennymiesner@users.noreply.github.com>
2021-08-09 16:21:09 -0500

    Connected the reducer and UI is displaying our state

--- src/App.js
+++ src/App.js
@@ -5,8 +5,10 @@ import './App.css';
 
 import TotalDisplay from './components/TotalDisplay';
 import CalcButton from './components/CalcButton';
+import { addOne } from './actions'
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState)
 
   return (
     <div className="App">
@@ -18,10 +20,10 @@ function App() {
         <div className="col-md-12 d-flex justify-content-center">
           <form name="Cal">
             
-            <TotalDisplay value={0}/>
+            <TotalDisplay value={state.total}/>
             <div className="row details">
-              <span id="operation"><b>Operation:</b> X</span>
-              <span id="memory"><b>Memory:</b> 0</span>
+              <span id="operation"><b>Operation:</b> {state.operation}</span>
+              <span id="memory"><b>Memory:</b> {state.memory}</span>
             </div>
             
             <div className="row">

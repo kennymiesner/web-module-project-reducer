6521cd7e5350f436a1938484a86dc77dcc3f9233
Kenny Miesner <84544990+kennymiesner@users.noreply.github.com>
2021-08-09 19:47:02 -0500

    Adds display clear

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './App.css'
 
 import TotalDisplay from './components/TotalDisplay'
 import CalcButton from './components/CalcButton'
-import { applyNumber, changeOperation } from './actions'
+import { applyNumber, changeOperation, clearDisplay } from './actions'
 
 function App() {
   const [state, dispatch] = useReducer(reducer, initialState)
@@ -17,6 +17,10 @@ function App() {
 
   const handleOperation = (operator) => {
     dispatch(changeOperation(operator))
+  }
+
+  const handleClearDisplay = () => {
+    dispatch(clearDisplay())
   }
 
   return (
@@ -66,7 +70,7 @@ function App() {
             </div>
 
             <div className="row ce_button">
-              <CalcButton value={"CE"}/>
+              <CalcButton value={"CE"} onClick={handleClearDisplay}/>
             </div>
 
           </form>
